b17c4b651d22a2fbcfb2463285261f5920b22fe2
Douglas Ernandes <DouglasErnandes87@gmail.com>
2026-01-21 17:31:08 +0000

    Remova esta seção Onde está escrito "Nossa história" (_for element div_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,11 +16,6 @@ export default function Home() {
       <main className="flex-1">
         <div data-theme="dark">
           <Hero />
-          <div className="bg-secondary py-12 px-4 text-center bg-textured">
-            <h2 className="text-3xl font-serif font-bold italic text-white sm:text-4xl md:text-5xl">
-              Nossa História
-            </h2>
-          </div>
           <About />
           <MissionVisionValues />
         </div>
