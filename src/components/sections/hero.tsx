1173463793db72ceca57d4ea95bb3f8c4bfbf5ae
Douglas Ernandes <DouglasErnandes87@gmail.com>
2026-01-22 00:05:57 +0000

    Continue no mesmo padrão, mas cresca agora somente mais 10%.

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -7,12 +7,12 @@ export function Hero() {
     <section id="hero" className="relative h-[80vh] md:h-screen text-primary-foreground bg-primary overflow-hidden">
       
       {/* Positioned Logo */}
-      <div className="absolute bottom-0 left-0 w-11/12 max-w-6xl">
+      <div className="absolute bottom-0 left-0 w-full max-w-6xl">
         <Image
           src="https://i.imgur.com/fROlhwD.png"
           alt="Nilo Travesseiros Logo"
-          width={1380}
-          height={1380}
+          width={1280}
+          height={1280}
           className="object-contain"
           priority
         />
