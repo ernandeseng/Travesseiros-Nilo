e1987b056b4a145d76da2224332e21e0b67015be
Douglas Ernandes <DouglasErnandes87@gmail.com>
2026-01-22 00:03:20 +0000

    Continue crescendo a imagem no mesmo padrão que você fez, aumente mais 3

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -7,12 +7,12 @@ export function Hero() {
     <section id="hero" className="relative h-[80vh] md:h-screen text-primary-foreground bg-primary overflow-hidden">
       
       {/* Positioned Logo */}
-      <div className="absolute bottom-0 left-0 w-10/12 max-w-4xl">
+      <div className="absolute bottom-0 left-0 w-11/12 max-w-6xl">
         <Image
           src="https://i.imgur.com/fROlhwD.png"
           alt="Nilo Travesseiros Logo"
-          width={1024}
-          height={1024}
+          width={1380}
+          height={1380}
           className="object-contain"
           priority
         />
