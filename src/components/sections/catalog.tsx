19f39cac82e9077e9d39b33101fb929db88991de
Douglas Ernandes <DouglasErnandes87@gmail.com>
2026-01-23 01:43:33 +0000

    Faça com que as imagens que estão nos catálogos não fiquem com aquela fa

--- src/components/sections/catalog.tsx
+++ src/components/sections/catalog.tsx
@@ -71,7 +71,7 @@ export function Catalog() {
                                       alt={product.name}
                                       fill
                                       data-ai-hint={image.imageHint}
-                                      className="rounded-t-lg object-cover"
+                                      className="object-contain p-4"
                                     />
                                   )}
                                 </CardHeader>
