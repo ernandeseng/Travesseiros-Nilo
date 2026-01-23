651a0440b50c9a9afa1ffa1fe865b22db0523adb
Douglas Ernandes <DouglasErnandes87@gmail.com>
2026-01-23 21:20:12 +0000

    Remova este card "Almofada Personalizada Infantil" (_for element <Card>_

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -55,7 +55,6 @@ export const productsByCategory = {
         { id: "018", name: "Almofada Suede Lisa", imageId: 'almofada-suede-lisa-018' },
         { id: "020", name: "Almofada Estampada", imageId: 'product3' },
         { id: "039", name: "Almofada Personalizada de Time", imageId: 'product5' },
-        { id: "037", name: "Almofada Personalizada Infantil", imageId: 'product6' },
         { id: "042", name: "Almofadas Suede 45x45", imageId: "almofada-suede-45x45-042" },
         { id: "043", name: "Almofadas Matelada 45x45", imageId: "almofada-matelada-45x45-043" },
         { id: "046", name: "Almofada Personalizada", imageId: "almofada-personalizada-046" },
