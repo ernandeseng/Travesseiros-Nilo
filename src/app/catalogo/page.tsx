7b7fa9ec5f072a028560d9e58185fd63259351e8
Douglas Ernandes <DouglasErnandes87@gmail.com>
2026-01-26 21:46:49 +0000

    Somente para as imagens do catálogo completo na versão MOBILE apenas:
    C

--- src/app/catalogo/page.tsx
+++ src/app/catalogo/page.tsx
@@ -103,7 +103,7 @@ export default function CatalogoPage() {
                 key={product.id}
                 className="text-left flex flex-col group overflow-hidden"
               >
-                <CardHeader className={cn("p-0 overflow-hidden aspect-square")}>
+                <CardHeader className={cn("p-0 overflow-hidden md:aspect-square")}>
                   {image && (
                     <Image
                       src={image.imageUrl}
@@ -111,7 +111,7 @@ export default function CatalogoPage() {
                       width={500}
                       height={500}
                       data-ai-hint={image.imageHint}
-                      className={cn("w-full h-full object-cover")}
+                      className={cn("w-full h-auto object-scale-down block md:h-full md:object-cover")}
                     />
                   )}
                 </CardHeader>
